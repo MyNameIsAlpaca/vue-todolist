3d234ca0f90d80a0c771bcf2b8e0055add22a07d
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-03-15 14:09:35 +0100

    feat: add task and x

--- js/script.js
+++ js/script.js
@@ -3,7 +3,30 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      
+      todos: [
+        {
+            task: "portare fuori il cane",
+            done: false,
+        },
+        {
+            task: "scrivere un libro",
+            done: false,
+        },
+      ],
+      taskValue: '',
     }
-  }
+  },
+  methods: {
+    addTask() {
+        
+        this.newTask = {
+            task: this.taskValue,
+            done: false,
+        };
+
+        this.todos.push(this.newTask);
+        
+        this.taskValue = '';
+    }
+  },
 }).mount('#app')
